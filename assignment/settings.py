aaf6167f83441e920c0eed14bb23f71e5c33ed5b
Swapnil Rana <50835521+swapy-S@users.noreply.github.com>
2021-06-20 23:15:24 +0530

    Update settings.py

--- assignment/settings.py
+++ assignment/settings.py
@@ -116,6 +116,7 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
